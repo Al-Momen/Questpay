c32905e2b54b55cc07f301a2a4dfd317f1c1dd1f
AL-Momen <58368636+Al-Momen@users.noreply.github.com>
2025-10-16 20:41:58 +0600

    survey search

--- application/app/Http/Controllers/SiteController.php
+++ application/app/Http/Controllers/SiteController.php
@@ -58,22 +58,30 @@ class SiteController extends Controller
         return view('Template::blog_details', compact('blog', 'pageTitle', 'latests'));
     }
 
-    public function survey()
+    public function survey(Request $request)
     {
+
         $pageTitle  = "Survey";
-        $surveys    = Survey::with('category','deposit')->where('status', Status::SURVEY_ENABLE)->paginate(getPaginate());
+        $categoryIds = $request->input('category_id');
+        $surveys = Survey::with(['category', 'deposit'])
+            ->searchable(['title'])
+            ->where('status', Status::SURVEY_ENABLE)
+            ->when(!empty($categoryIds), function ($q) use ($categoryIds) {
+                $q->whereIn('category_id', $categoryIds);
+            })
+            ->paginate(getPaginate());
         $sections   = Page::where('slug', 'survey')->first();
         $categories = Category::where('status', Status::CATEGORY_ENABLE)->get();
-        return view('Template::survey', compact('pageTitle', 'sections','surveys','categories'));
+        return view('Template::survey', compact('pageTitle', 'sections', 'surveys', 'categories'));
     }
 
     public function surveyDetails($id)
     {
         $pageTitle  = "Survey Details";
-        $survey     = Survey::with('category','deposit')->where('status', Status::SURVEY_ENABLE)->where('id',$id)->first();
+        $survey     = Survey::with('category', 'deposit')->where('status', Status::SURVEY_ENABLE)->where('id', $id)->first();
         $sections   = Page::where('slug', 'survey')->first();
         $categories = Category::where('status', Status::CATEGORY_ENABLE)->get();
-        return view('Template::survey_details', compact('pageTitle', 'sections','survey','categories'));
+        return view('Template::survey_details', compact('pageTitle', 'sections', 'survey', 'categories'));
     }
 
     public function about()
--- application/resources/views/presets/default/survey.blade.php
+++ application/resources/views/presets/default/survey.blade.php
@@ -7,54 +7,68 @@
                     <div class="filter__wrap">
                         <div class="survey-list__bottom-main d-flex justify-content-between flex-wrap">
                             <h3 class="deshboard-bottom__title m-0">@lang('Survey')</h3>
-                            <div class=" input-search__box d-flex gap--12">
-                                <div class="search__box  position-relative">
-                                    <input type="text" class="form-control bottom-search__box"
-                                        placeholder="@lang('Search Survey')...">
-                                    <button class="" type="submit">
-                                        <i class="fa-solid fa-magnifying-glass"></i>
-                                    </button>
-                                </div>
-                                <div class="filter-wrap">
-                                    <span class="filter__btn">
-                                        <i class="fa-solid fa-filter"></i>
-                                    </span>
-                                    <div class="filter__main">
-                                        <div class="filter-meading__section">
-                                            <div class="text-end">
-                                                <span class="filter__close">
-                                                    <i class="fa-solid fa-xmark"></i>
-                                                </span>
-                                            </div>
-                                            <div
-                                                class="filter-heading d-flex justify-content-between align-items-center mt-3 mb-4">
-                                                <h3 class="filter-heading__title fs--20 fw--600 mb-0">@lang('Filter')</h3>
-                                                <p class="filter-heading__clear-btn cursor-pointer">@lang('Clear All')</p>
-                                            </div>
-                                            <div class="row g-4">
-                                                <div class="col-lg-12">
-                                                    <h4 class="category-title fs--16 fw--600 mb-2">@lang('Category')</h4>
-                                                    <div class="filter__form">
-                                                        @foreach ($categories ?? [] as $item)
-                                                            <div
-                                                                class="form-check d-flex justify-content-between align-items-center mb-4">
-                                                                <label class="form-check-label fs--16 text-black" for="{{$item->id}}">
-                                                                    {{__($item->name)}}
-                                                                </label>
-                                                                <input class="form-check-input" type="checkbox"
-                                                                    value="{{$item->id}}" id="{{$item->id}}">
-                                                            </div>
-                                                        @endforeach
+                            <form action="" post="get">
+                                <div class="input-search__box d-flex gap--12">
+                                    <div class="search__box  position-relative">
+                                        <input type="text" class="form-control bottom-search__box" name="search"
+                                            placeholder="@lang('Search Survey')..." value="{{ request()->search }}">
+                                        <button class="" type="submit">
+                                            <i class="fa-solid fa-magnifying-glass"></i>
+                                        </button>
+                                    </div>
+                                    <div class="filter-wrap">
+                                        <span class="filter__btn">
+                                            <i class="fa-solid fa-filter"></i>
+                                        </span>
+                                        <div class="filter__main">
+                                            <div class="filter-meading__section">
+                                                <div class="text-end">
+                                                    <span class="filter__close">
+                                                        <i class="fa-solid fa-xmark"></i>
+                                                    </span>
+                                                </div>
+                                                <div
+                                                    class="filter-heading d-flex justify-content-between align-items-center mt-3 mb-4">
+                                                    <h3 class="filter-heading__title fs--20 fw--600 mb-0">@lang('Filter')
+                                                    </h3>
+                                                    <p class="filter-heading__clear-btn cursor-pointer clearAll">
+                                                        @lang('Clear All')
+                                                    </p>
+                                                </div>
+                                                <div class="row g-4">
+                                                    <div class="col-lg-12">
+                                                        <h4 class="category-title fs--16 fw--600 mb-2">@lang('Category')
+                                                        </h4>
+                                                        @php
+                                                            $selectedCategories = request()->category_id ?? [];
+                                                        @endphp
+                                                        <div class="filter__form">
+                                                            @foreach ($categories ?? [] as $item)
+                                                                <div
+                                                                    class="form-check d-flex justify-content-between align-items-center mb-4">
+                                                                    <label class="form-check-label fs--16 text-black"
+                                                                        for="cat_{{ $item->id }}">
+                                                                        {{ __($item->name) }}
+                                                                    </label>
+
+                                                                    <input class="form-check-input" type="checkbox"
+                                                                        name="category_id[]" value="{{ $item->id }}"
+                                                                        id="cat_{{ $item->id }}"
+                                                                        {{ in_array($item->id, $selectedCategories) ? 'checked' : '' }}>
+                                                                </div>
+                                                            @endforeach
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="dashboard-list__button w--100 mt-4">
-                                                <a class="filter--btn w--100 fs--16 fw--600" href="#">@lang("Apply Now")</a>
+                                                <div class="dashboard-list__button w--100 mt-4">
+                                                    <button class="filter--btn w--100 fs--16 fw--600"
+                                                        type="submit">@lang('Search')</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -63,12 +77,14 @@
                         @foreach ($surveys ?? [] as $item)
                             <div class="col-lg-4 col-md-6">
                                 <div class="dashboard-list__items">
-                                    <a href="#" class="dashboard-list__thumb mb-3 w--100">
-                                        <img class="w--100 radius--12" src="{{ getImage(getFilePath('survey') . '/' . $item->image) }}"
+                                    <a href="{{ route('survey.details', $item->id) }}"
+                                        class="dashboard-list__thumb mb-3 w--100">
+                                        <img class="w--100 radius--12"
+                                            src="{{ getImage(getFilePath('survey') . '/' . $item->image) }}"
                                             alt="@lang('dashboard-img')">
                                     </a>
                                     <div class="dashboard-list__content">
-                                        <a href="#">
+                                        <a href="{{ route('survey.details', $item->id) }}">
                                             <h3 class="dashboard-list__title fs--20 fw--700 ps-3">{{ $item->title }}</h3>
                                         </a>
                                         <div class="dashboard-list__catagory d-flex justify-content-between">
@@ -78,7 +94,8 @@
                                                         <i class="fa-solid fa-tag"></i>
                                                     </span>@lang('Category')
                                                 </p>
-                                                <p class="fs-16 fw--400 text--black ps-4">{{ __($item->category->name) }}</p>
+                                                <p class="fs-16 fw--400 text--black ps-4">{{ __($item->category->name) }}
+                                                </p>
                                             </div>
                                             <div class="dashboard-list__line"></div>
                                             <div class="right-tag pe-3">
@@ -99,7 +116,10 @@
                                                 <p class="dashboard-list__mouny-desc">@lang('Total Amount')</p>
                                             </div>
                                             <div class="dashboard-list__button">
-                                                <a class="btn btn--base custom--btn" href="{{route('survey.details',$item->id)}}">@lang('Start Survey')</a>
+                                                <a class="btn btn--base custom--btn"
+                                                    href="{{ route('survey.details', $item->id) }}">
+                                                    @lang('Start Survey')
+                                                </a>
                                             </div>
                                         </div>
                                     </div>
@@ -112,3 +132,16 @@
         </div>
     </section>
 @endsection
+
+@push('script')
+    <script>
+        (function($) {
+            'use strict';
+            $(document).ready(function() {
+                $('.clearAll').on('click', function() {
+                    $('input[type="checkbox"][name="category_id[]"]').prop('checked', false);
+                });
+            });
+        })(jQuery);
+    </script>
+@endpush
